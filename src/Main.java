58c5ebf2c90c8681431469b65f1241b8952d1332
omidamin12 <fnuomied@gmail.com>
2023-06-01 19:38:30 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "omid";
+        for (int i = 0; i < 10; i++) {
+
+
+            System.out.println(name);
+        }
+    }
+}
