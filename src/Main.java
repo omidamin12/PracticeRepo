7febd5899c0e005578862043ba4b8c11b78a6f45
omidamin12 <fnuomied@gmail.com>
2023-06-03 00:32:55 -0400

    Some Changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         String name = "omid";
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
+            Thread.sleep(2000);
 
 
             System.out.println(name);
